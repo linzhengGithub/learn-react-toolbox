578ae64ddd906af537b9341ccd946ca23d2c8f48
linzheng <alidlg9045@126.com>
2023-08-28 16:28:32 +0800

    update

--- learn-hoc-form/src/components/MyForm/index.js
+++ learn-hoc-form/src/components/MyForm/index.js
@@ -3,6 +3,7 @@ import _CreateForm from './CreateForm';
 import CreateField from './CreateField';
 import useForm from './useForm';
 
+// forwardRef 允许你的组件使用 ref 将一个 DOM 节点暴露给父组件
 const CreateForm = React.forwardRef(_CreateForm)
 
 CreateForm.Field = CreateField
