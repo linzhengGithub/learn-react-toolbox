c4a4911d7e201aa988db02ae107d84245a5c4022
linzheng <alidlg9045@126.com>
2023-09-25 23:51:50 +0800

    learn: router 基本使用

--- learn-rc-router6/src/App.js
+++ learn-rc-router6/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+// BrowserRouter / HashRouter
+// 前者需要服务端对不同URL返回不同的HTML,后端需配置 / 后者不需要服务端渲染,通过浏览器#来区分path就可以
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/product' element={<Product />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
+}
+
+function Layout() {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Link to="/">首页</Link>
+      <Link to="/product">商品</Link>
+
+      <Outlet />
+    </div>
+  )
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+function Product() {
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  )
 }
 
 export default App;
